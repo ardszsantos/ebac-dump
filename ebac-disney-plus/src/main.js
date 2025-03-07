5945d5cad38855d81ffb79d478941c67790b8320
ardszsantos <lokgamer01@gmail.com>
2025-03-07 13:12:44 -0300

    DONE

--- ebac-disney-plus/src/main.js
+++ ebac-disney-plus/src/main.js
@@ -2,6 +2,33 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]')
     const questions = document.querySelectorAll('[data-faq-question]')
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight
+
+
+    window.addEventListener("scroll", function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector("header");
+    header.classList.add('header--is-hidden');
+    
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector("header");
+    header.classList.remove('header--is-hidden');
+    
+}
+
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,8 +39,6 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
-
-
 
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
@@ -43,3 +68,4 @@ function abreOuFechaResposta(elemento) {
     elementoPai.classList.toggle(classe)
     
 }
+
