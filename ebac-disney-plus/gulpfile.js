b5be355f8ff928a0fa20f9f7590cf32fb96d1684
ardszsantos <lokgamer01@gmail.com>
2025-04-15 16:41:20 -0300

    .

--- ebac-disney-plus/gulpfile.js
+++ ebac-disney-plus/gulpfile.js
@@ -3,6 +3,15 @@ const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
 const mozjpeg = require('imagemin-mozjpeg');
 const pngquant = require('imagemin-pngquant');
+const uglify = require('gulp-uglify');
+
+
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 
 function styles() {
@@ -23,8 +32,9 @@ function images() {
 }
 
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
 
