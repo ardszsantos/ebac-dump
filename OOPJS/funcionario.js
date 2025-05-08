fac0730cf68bda4b8c0c156dc3e191326c21a92b
ardszsantos <lokgamer01@gmail.com>
2025-05-08 16:05:45 -0300

    using getters and setters funcs

--- OOPJS/funcionario.js
+++ OOPJS/funcionario.js
@@ -15,13 +15,22 @@ function Funcionario(nome, cargo, salario) {
     this.nome = nome;
     this.cargo = cargo;
     let _salario = salario;
-    this.retornaSalario = function () {
+    this.getSalario = function () {
         return _salario;
+    }
+
+    this.setSalario = function (valor) {
+        if (typeof valor === 'number') {
+            _salario += valor;
+        }
     }
     Pessoa.call(this, nome);
 }
 
 const funcionario1 = new Funcionario("Maria", "dev", 5000);
+
+funcionario1.setSalario(805555000)
+
 
 
 console.log(funcionario1.retornaSalario())
