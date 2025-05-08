84ee3308085fb45a1c33f690f732ca8f8f92d6e9
ardszsantos <lokgamer01@gmail.com>
2025-05-08 15:57:53 -0300

    .

--- OOPJS/main.js
+++ OOPJS/main.js
@@ -17,3 +17,23 @@ const carroDoG = new Carro("Ka", "Ford", 2021, 2020);
 
 console.log(carroDoG)
 console.log(carroDoJot)
+
+const nome = "gian";
+const idade = 30;
+const eMaiorDeIdade = true;
+const conhecimentos = ["html", "css", "javascript"];
+
+
+
+const pessoa = {
+    nome: nome,
+    idade: idade,
+    eMaiorDeIdade: eMaiorDeIdade,
+    conhecimentos: conhecimentos,
+}
+
+pessoa.sobrenome = 'souza';
+
+
+
+console.log(pessoa.sobrenome)
