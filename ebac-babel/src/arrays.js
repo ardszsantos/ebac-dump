3e488852ac1f3b71ff46fc976616151284a7916a
ardszsantos <lokgamer01@gmail.com>
2025-05-13 15:17:19 -0300

    .

--- ebac-babel/src/arrays.js
+++ ebac-babel/src/arrays.js
@@ -1,5 +1,33 @@
 const redesSociais =  ['Facebook', 'Instagram', 'Twitter'];
 
-for (let i = 0; i < redesSociais.length; i++) {
-    console.log(`Eu tenho perfil na rede social: ${redesSociais[i]}`)
-} 
+
+redesSociais.forEach(function (redeSNome, i) {
+    console.log(` #${i + 1} Eu tenho perfil na rede social: ${redeSNome}`)
+}) 
+
+
+
+const alunos = ['Gustavo', 'Julia', 'Paula', 'Vagner']
+
+const alunos2 = alunos.map(function(itemAtual) {
+
+
+    return {
+        nome: itemAtual,
+        curso: 'Frontend',
+    };
+})
+
+
+
+
+console.log(alunos2)
+
+
+const numeros = [1, 2, 3, 4, 5]
+const dobroDosNumeros = numeros.map(function(numeroAtual) {
+    return numeroAtual * 2;
+})
+
+
+console.log(dobroDosNumeros)
