65fde290f22106d201a11773d404074a12592047
ardszsantos <lokgamer01@gmail.com>
2025-05-24 15:52:57 -0300

    learning rest

---
+++ ebac-babel/dist/rest_spread.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function somar(a, b) {
+  var soma = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    soma += arguments[i];
+  }
+  return soma;
+}
+console.log(somar(20, 20, 30));
+function somarComRest() {
+  for (var _len = arguments.length, numeros = new Array(_len), _key = 0; _key < _len; _key++) {
+    numeros[_key] = arguments[_key];
+  }
+  var soma = numeros.reduce(function (total, numeroAtual) {
+    total += numeroAtual;
+    return total;
+  }, 0);
+  return soma;
+}
+console.log(somarComRest(10, 20, 30, 40));
