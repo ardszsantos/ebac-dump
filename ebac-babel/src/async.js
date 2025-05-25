6be2ab5eb466d16227d886b42a91a6ec1c157d24
ardszsantos <lokgamer01@gmail.com>
2025-05-25 17:09:40 -0300

    async e await

--- ebac-babel/src/async.js
+++ ebac-babel/src/async.js
@@ -20,8 +20,38 @@ const funcaoMuitoPesadaPromise = new Promise((resolve, reject)  => {
         reject("Deu erro em algum mocó ai");
     }
 })
-console.log("inicio")
-//console.log(funcaoMuitoPesada())
-funcaoMuitoPesadaPromise.then(resultado => console.log(resultado))
 
-console.log("fim")
+//console.log("inicio")
+//console.log(funcaoMuitoPesada())
+//funcaoMuitoPesadaPromise.then(resultado => console.log(resultado)).catch(erro => console.log(erro))
+//console.log("fim")
+
+
+
+
+const promiseComParametros = (login, senha) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Logado com o usuário ${login}`)
+        }, 3000)
+    })
+}
+
+
+
+async function execucaoPrincipal() {
+    console.log("inicio")
+
+
+    promiseComParametros("gian@gmail.com", 123456).then((resultado) => {
+        console.log(resultado);
+    })
+    try {
+        const resultado = await funcaoMuitoPesadaPromise;
+        console.log(resultado)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+execucaoPrincipal();
