e806d5fdea1e6f2c9c41ebb4c53bc5d73c49e66b
ardszsantos <lokgamer01@gmail.com>
2025-05-25 17:00:34 -0300

    n

---
+++ ebac-babel/src/async.js
@@ -0,0 +1,25 @@
+function funcaoMuitoPesada() {
+    let execucoes = 0;
+
+    for (let i = 0; i < 1000000000; i++) {
+        execucoes++
+    }
+    return execucoes
+    
+}
+
+const funcaoMuitoPesadaPromise = new Promise((resolve, reject)  => {
+    try {
+        let execucoes = 0;
+
+        for (let i = 0; i < 1000000000; i++) {
+            execucoes++
+        }
+        resolve(execucoes);
+    } catch {
+        reject("Deu erro em algum mocó ai");
+    }
+})
+console.log("inicio")
+console.log(funcaoMuitoPesada())
+console.log("fim")
