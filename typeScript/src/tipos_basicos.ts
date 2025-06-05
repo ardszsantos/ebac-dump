2beaa9037fa15f352508cf3d62b1580b9ff00cac
ardszsantos <lokgamer01@gmail.com>
2025-06-05 15:06:10 -0300

    .

---
+++ typeScript/src/tipos_basicos.ts
@@ -0,0 +1,20 @@
+let estaChovendo: boolean = false;
+
+
+
+let idade: number = 27;
+
+let altura: number = 1.75;
+
+const nacionalidade: string = 'parma'
+
+const colegas: string[] = ['lucas', 'fernamda' ]
+const tecnologias: Array<string> = ['html', 'css', 'js']
+const notas : ReadonlyArray<number> = [7, 9, 5, 8]
+
+const lista: [nome: string, griew: boolean, idade: number] = ['gian', true, 27];
+
+let idadeDaAna: number | string = 25;
+
+let dadosDaApi: any;
+
