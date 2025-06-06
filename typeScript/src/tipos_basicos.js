17ba0e7d517ab2ff480451e607f38deb1e4edaa0
ardszsantos <lokgamer01@gmail.com>
2025-06-06 18:10:35 -0300

    .

---
+++ typeScript/src/tipos_basicos.js
@@ -0,0 +1,11 @@
+"use strict";
+let estaChovendo = false;
+let idade = 27;
+let altura = 1.75;
+const nacionalidade = 'parma';
+const colegas = ['lucas', 'fernamda'];
+const tecnologias = ['html', 'css', 'js'];
+const notas = [7, 9, 5, 8];
+const lista = ['gian', true, 27];
+let idadeDaAna = 25;
+let dadosDaApi;
